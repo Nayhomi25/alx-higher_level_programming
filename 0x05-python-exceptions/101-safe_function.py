a694742cc7c618de459851596daacc82387f08ec
Osarieme Naomi Osunde <naomiosariemeosunde@gmail.com>
2022-10-10 21:10:40 +0100

    Update 101-safe_function.py

--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -7,5 +7,5 @@ def safe_function(fct, *args):
         return (result)
     except Exception as error:
         import sys
-        print("Exceptionr {}".format(sys.exc_info()[1]), file=sys.stderr)
+        print("Exception: {}".format(error), file=sys.stderr)
         return (None)
