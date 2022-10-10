31eac7757a68f0e92cc093f53cc2fc025b44b8c5
Osunde Osarieme Naomi <naomiosariemeosunde@gmail.com>
2022-10-10 12:57:46 -0700

    fix

--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
 import sys
+
+
 def safe_function(fct, *args):
     try:
         result = fct(*args)
         return (result)
-    except:
-        print("Exception: {}".format(sys.exc_info()[1]), file=sys.stderr)
+    except TypeError:
+        print("Exceptionr {}".format(sys.exc_info()[1]), file=sys.stderr)
         return (None)
