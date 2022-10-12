6a22433fc56369480db81769df3154e73c2c3d33
Osunde Osarieme Naomi <naomiosariemeosunde@gmail.com>
2022-10-11 18:18:27 -0700

    private atrribute size

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,9 @@
+#!/user/bin/python3
+""" filename: 1-square.py """
+
+
+class Square:
+    """ A class Square that defines a square by size """
+    def __init__(self, size=None):
+        """ Initializes a private attribute called size """
+        self.__size = size
