e72445419b07c945d4a79355441d167142a192ee
Osunde Osarieme Naomi <naomiosariemeosunde@gmail.com>
2023-05-15 15:33:10 -0700

    pycodestyle

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -4,6 +4,6 @@ def print_reversed_list_integer(my_list=[]):
     if isinstance(my_list, list):
         tsil = my_list
         tsil.reverse()
-        
+
         for x in tsil:
             print("{:d}".format(x))
