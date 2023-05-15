a788b7821d40ffcc3d3aa3f4bb5f3d4f455c6e6b
Osunde Osarieme Naomi <naomiosariemeosunde@gmail.com>
2023-05-15 15:40:12 -0700

    i did not make a copy of the list

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
 
 def new_in_list(my_list, idx, element):
+    fake_list = my_list.copy()
+
     if idx < 0 or idx > len(my_list) - 1:
-        return(my_list[:])
+        return(fake_list)
     else:
-        my_list[idx] = element
-        return(my_list)
+        fake_list[idx] = element
+    return fake_list
