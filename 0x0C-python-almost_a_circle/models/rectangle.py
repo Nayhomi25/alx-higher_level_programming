36ef6340701bb40447355eb69462f03ab2b1e614
Osarieme Naomi Osunde <naomiosariemeosunde@gmail.com>
2023-06-19 19:43:57 +0100

    Fixing errors

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -21,11 +21,13 @@ class Rectangle(Base):
 
     @width.setter
     def width(self, value):
-        """set the value of the width attribute"""
-        if type(value) != int:
+        """Sets the value for width"""
+        if (type(value) is not int):
             raise TypeError("width must be an integer")
+
         if value <= 0:
             raise ValueError("width must be > 0")
+
         self.__width = value
 
     @property
@@ -35,11 +37,13 @@ class Rectangle(Base):
 
     @height.setter
     def height(self, value):
-        """set the value of the height attribute"""
-        if type(value) != int:
+        """Sets the value for height"""
+        if (type(value) is not int):
             raise TypeError("height must be an integer")
+
         if value <= 0:
             raise ValueError("height must be > 0")
+
         self.__height = value
 
     @property
@@ -49,25 +53,29 @@ class Rectangle(Base):
 
     @x.setter
     def x(self, value):
-        """set the value of the x attribute"""
-        if type(value) != int:
+        """Sets the value for x"""
+        if (type(value) is not int):
             raise TypeError("x must be an integer")
-        if value <= 0:
-            raise ValueError("x must be >= 0")
-        self.__x = value
 
+        if value < 0:
+            raise ValueError("x must be >= 0")
+
+        self.__x = value
+        
     @property
     def y(self):
         """retrives the y attribute"""
         return self.__y
 
-    @y.setter
+     @y.setter
     def y(self, value):
-        """set the value of the y attribute"""
-        if type(value) != int:
+        """Sets the value for y"""
+        if (type(value) is not int):
             raise TypeError("y must be an integer")
-        if value <= 0:
+
+        if value < 0:
             raise ValueError("y must be >= 0")
+
         self.__y = value
 
     def area(self):
@@ -92,8 +100,9 @@ class Rectangle(Base):
                                                        self.x, self.y,
                                                        self.width, self.height)
 
-    def update(self, *args):
-        """Assign an argument to each attribute"""
+    def update(self, *args, **kwargs):
+        """Assigns an argument to each attribute"""
+
         if args and len(args) != 0:
             a = 0
             for arg in args:
@@ -129,11 +138,10 @@ class Rectangle(Base):
                     self.y = v
 
     def to_dictionary(self):
-        """Return the dictionary representation of a Rectangle."""
-        return {
-            "id": self.id,
-            "width": self.width,
-            "height": self.height,
-            "x": self.x,
-            "y": self.y
-        }
+        """Returns the dictionary representation of a Rectangle"""
+
+        obj_dictionary = {'id': self.id, 'width': self.__width,
+                          'height': self.__height, 'x': self.__x,
+                          'y': self.__y}
+
+        return obj_dictionary
