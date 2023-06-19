7d419e9f019f5d13fafb80c5c0807434ecfdaf2d
Osarieme Naomi Osunde <naomiosariemeosunde@gmail.com>
2023-06-19 19:37:51 +0100

    Update rectangle.py

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -24,7 +24,7 @@ class Rectangle(Base):
         """set the value of the width attribute"""
         if type(value) != int:
             raise TypeError("width must be an integer")
-        if value < 0:
+        if value <= 0:
             raise ValueError("width must be > 0")
         self.__width = value
 
@@ -38,7 +38,7 @@ class Rectangle(Base):
         """set the value of the height attribute"""
         if type(value) != int:
             raise TypeError("height must be an integer")
-        if value < 0:
+        if value <= 0:
             raise ValueError("height must be > 0")
         self.__height = value
 
@@ -52,7 +52,7 @@ class Rectangle(Base):
         """set the value of the x attribute"""
         if type(value) != int:
             raise TypeError("x must be an integer")
-        if value < 0:
+        if value <= 0:
             raise ValueError("x must be >= 0")
         self.__x = value
 
@@ -66,7 +66,7 @@ class Rectangle(Base):
         """set the value of the y attribute"""
         if type(value) != int:
             raise TypeError("y must be an integer")
-        if value < 0:
+        if value <= 0:
             raise ValueError("y must be >= 0")
         self.__y = value
 
