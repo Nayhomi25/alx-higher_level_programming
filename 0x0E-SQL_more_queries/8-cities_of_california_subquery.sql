1ab6a96496edeb882f9019c3698cf1f5968a2b21
Osunde Osarieme Naomi <naomiosariemeosunde@gmail.com>
2023-07-20 14:22:26 -0700

    mysql joins

---
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -0,0 +1,9 @@
+-- lists all the cities of California
+-- Results are ordered by ascending cities_id 
+SELECT `id`, `name`
+  FROM `cities`
+ WHERE `state_id` IN
+       (SELECT `id`
+	  FROM `states`
+	 WHERE `name` = "California")
+ ORDER BY `id`;
