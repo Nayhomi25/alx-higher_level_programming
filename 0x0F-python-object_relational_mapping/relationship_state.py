1e6cfc3874b9ee4c864b2f327ce3fc669098b753
Osunde Osarieme Naomi <naomiosariemeosunde@gmail.com>
2023-08-20 17:11:56 -0700

    fixing errors

--- 0x0F-python-object_relational_mapping/relationship_state.py
+++ 0x0F-python-object_relational_mapping/relationship_state.py
@@ -2,12 +2,11 @@
 """
 Contains State class and Base, an instance of declarative_base()
 """
-from sqlalchemy import Column, Integer, String, MetaData
+from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
 
-mymetadata = MetaData()
-Base = declarative_base(metadata=mymetadata)
+Base = declarative_base()
 
 
 class State(Base):
@@ -17,4 +16,4 @@ class State(Base):
     __tablename__ = 'states'
     id = Column(Integer, unique=True, nullable=False, primary_key=True)
     name = Column(String(128), nullable=False)
-    cities = relationship("City", backref="states")
+    cities = relationship("City", backref="state", cascade="all, delete")
