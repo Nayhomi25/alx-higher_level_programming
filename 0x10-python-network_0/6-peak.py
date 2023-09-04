29a95cedc60c134611da4fd9c5bfb41c037bb479
Osunde Osarieme Naomi <naomiosariemeosunde@gmail.com>
2023-09-04 09:22:52 -0700

    pycodestyle violation

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -3,7 +3,7 @@
 
 
 def find_peak(list_of_integers):
-"""Return a peak in a list of unsorted integers."""
+    """Return a peak in a list of unsorted integers."""
     if list_of_integers is None or len(list_of_integers) == 0:
         return None
 
